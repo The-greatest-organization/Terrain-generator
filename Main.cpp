4c57ff98242ef2f9f7b9b1c03fdee54f84c6beb4
AndrewLa <andrew.lzrv@gmail.com>
2024-03-27 00:03:32 +0700

    Cmake script was a bit corrected

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,27 @@
 #include <GLFW/glfw3.h>
+#include <cassert>
 
 int main(int argc, char** argv) {
-    if (!glfwInit()) {
+    int code = glfwInit();
+
+    if (code == -1) {
         return -1;
-    }
-    
+    }   
+
     GLFWwindow* window = glfwCreateWindow(1920, 1080, "Terrain generator", glfwGetPrimaryMonitor(), NULL);
-    if (!window) {
+
+    if (window == nullptr) {
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glClearColor(1, 1, 0, 1);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
