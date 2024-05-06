9e015d81016ebcb6a1a8814a118dc9e6698feb90
Егор <andreevegor10000@gmail.com>
2024-05-06 23:00:12 +0700

    forgot to push better ref

--- terrain_generator/run_ref.cpp
+++ terrain_generator/run_ref.cpp
@@ -3,12 +3,14 @@
 
 int main() {
     TerrainParams params{1, 1, 100};
-    Terrain tr{};
+    params.smoothing_hydraulic_erosion_steps = 0;
+    params.real_hydraulic_erosion_steps = 15;
+    Terrain tr{params};
 //    tr.params.resolution = 1;//edit params
-//    tr.generate();
+    tr.generate();
 //    tr.export_file("output", true);
-//    tr.export_png();
-    tr.import_file("output");
     tr.export_png();
+//    tr.import_file("output");
+//    tr.export_png();
     return 0;
 }
